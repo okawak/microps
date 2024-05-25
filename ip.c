66dc5bcf0493caf64568d23ecb35869ed9631a60
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 06:20:08 +0900

    Skeleton of step5

--- ip.c
+++ ip.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "platform.h"
+
 #include "ip.h"
 #include "net.h"
 #include "util.h"
@@ -15,6 +17,12 @@
 
 const ip_addr_t IP_ADDR_ANY = 0x00000000;       /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
+
+/*
+ * NOTE: if you want to add/delete the entries after net_run(),
+ *       you need to protect these lists with a mutex.
+ */
+static struct ip_iface *ifaces;
 
 int ip_addr_pton(const char *p, ip_addr_t *n) {
   char *sp, *ep;
@@ -46,6 +54,15 @@ char *ip_addr_ntop(ip_addr_t n, char *p, size_t size) {
   snprintf(p, size, "%d.%d.%d.%d", u8[0], u8[1], u8[2], u8[3]);
   return p;
 }
+
+struct ip_iface *ip_iface_alloc(const char *unicast, const char *netmask) {}
+
+/*
+ * NOTE: must not be call after net_run()
+ */
+int ip_iface_register(struct net_device *dev, struct ip_iface *iface) {}
+
+struct ip_iface *ip_iface_select(ip_addr_t addr) {}
 
 static void ip_print(const uint8_t *data, size_t len) {
   struct ip_hdr *hdr;
