889c9228dd84694fd9deb75ef1321c42a0f5e2cb
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 07:27:07 +0900

    Skeleton of step7

--- ip.c
+++ ip.c
@@ -18,6 +18,12 @@
 
 #define IP_HDR_OFFSET_MASK 0x1fff
 
+struct ip_protocol {
+  struct ip_protocol *next;
+  uint8_t protocol;
+  ip_protocol_handler_t handler;
+};
+
 const ip_addr_t IP_ADDR_ANY = 0x00000000;       /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
@@ -26,6 +32,7 @@ const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
  *       you need to protect these lists with a mutex.
  */
 static struct ip_iface *ifaces;
+static struct ip_protocol *protocols;
 
 int ip_addr_pton(const char *p, ip_addr_t *n) {
   char *sp, *ep;
@@ -112,6 +119,11 @@ struct ip_iface *ip_iface_select(ip_addr_t addr) {
   }
   return entry;
 }
+
+/*
+ * NOTE: must not be call after net_run()
+ */
+int ip_protocol_register(uint8_t protocol, ip_protocol_handler_t handler) {}
 
 static void ip_print(const uint8_t *data, size_t len) {
   struct ip_hdr *hdr;
