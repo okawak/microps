28e8079a94899b48e4e269d875e65bcba473d48e
okawak <kodak2022@outlook.jp>
2026-01-03 16:42:03 +0900

    Completion of step6

--- test/test.c
+++ test/test.c
@@ -14,8 +14,6 @@
 
 static volatile sig_atomic_t terminate;
 
-static struct net_device *dev;
-
 static void on_signal(int signum) {
   (void)signum;
   terminate = 1;
@@ -23,6 +21,7 @@ static void on_signal(int signum) {
 
 static int setup(void) {
   struct sigaction sa = {0};
+  struct net_device *dev;
   struct ip_iface *iface;
 
   sa.sa_handler = on_signal;
@@ -66,11 +65,16 @@ static int cleanup(void) {
 }
 
 static int app_main(void) {
+  ip_addr_t src, dst;
+  size_t offset = IP_HDR_SIZE_MIN;
+
+  ip_addr_pton(LOOPBACK_IP_ADDR, &src);
+  dst = src;
   debugf("press Ctrl+C to terminate");
   while (!terminate) {
-    if (net_device_output(dev, NET_PROTOCOL_TYPE_IP, test_data,
-                          sizeof(test_data), NULL) == -1) {
-      errorf("net_device_output() failure");
+    if (ip_output(1, test_data + offset, sizeof(test_data) - offset, src,
+                  dst) == -1) {
+      errorf("ip_output() failure");
       break;
     }
     sleep(1);
