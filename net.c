e97940d693c9bbc012ab090b219b06d7be3ed700
okawak <kodak2022@outlook.jp>
2026-01-04 11:17:55 +0900

    Completion of step7

--- net.c
+++ net.c
@@ -4,6 +4,7 @@
 
 #include "platform.h"
 
+#include "icmp.h"
 #include "ip.h"
 #include "net.h"
 #include "util.h"
@@ -183,6 +184,10 @@ int net_init(void) {
   }
   if (ip_init() == -1) {
     errorf("ip_init() failure");
+    return -1;
+  }
+  if (icmp_init() == -1) {
+    errorf("icmp_init() failure");
     return -1;
   }
   infof("success");
